14cc50802e90c7b40416d0d0cc0a438b8b15f8ff
Travis McGahey <travis.mcgahey@gmail.com>
2014-04-18 16:58:54 -0400

    Sorted leader board

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,5 +1,6 @@
 class LeadersController < ApplicationController
   def index
     @pool_members = PoolMember.all
+    @sorted_pool_members = @pool_members.sort_by { |pool_member| pool_member.total_team_points}.reverse
   end
 end
