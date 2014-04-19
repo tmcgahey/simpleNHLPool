d8602ba1cf59a09a8228d514cdd42a946471af3d
Travis McGahey <travis.mcgahey@gmail.com>
2014-04-18 23:41:38 -0400

    can retrieve skater data

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,4 +1,5 @@
 class LeadersController < ApplicationController
+
   def index
     @pool_members = PoolMember.all
     @sorted_pool_members = @pool_members.sort_by { |pool_member| pool_member.total_team_points}.reverse
@@ -6,11 +7,29 @@ class LeadersController < ApplicationController
 
   def updateSkaters
 
+    playoff_teams.each do |team|
+      @response = RestClient.get 'http://nhlwc.cdnak.neulion.com/fs1/nhl/league/playerstatsline/20132014/3/' + team + '/iphone/playerstatsline.json'
+
+      @json_response = ActiveSupport::JSON.decode(@response)
+
+      @json_response["skaterData"].each do |skater|
+        @skater_data = skater["data"].split(/,/)
+        Skater.create(name: @skater_data[2], pos: @skater_data[1],
+                      goals: @skater_data[4], assists: @skater_data[5],
+                      nhl_id: skater["id"], team: team)
+      end
+    end
+
+
     respond_to do |format|
-      format.html { redirect_to leaders_index_path }
+      format.html { redirect_to leaders_index_path, notice: "Skater data updated" }
       format.json { head :no_content }
     end
 
   end
 
+  private
+    def playoff_teams
+      @playoff_teams = ["DAL","ANA","MTL","TBL","DET","BOS","PHI","NYR","CHI","STL","MIN","COL","LAK","SJS","PIT","CBJ"]
+    end
 end
